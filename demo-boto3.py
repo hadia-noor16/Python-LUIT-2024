63132cb1cc63a28c86b26d9bea17b08799c4574d
hadia-noor16 <68270205+hadia-noor16@users.noreply.github.com>
2024-06-25 19:35:27 -0600

    Update demo-boto3.py

--- demo-boto3.py
+++ demo-boto3.py
@@ -1,8 +1,12 @@
 import boto3
+
+# Initialize an S3 client
 s3 = boto3.client('s3')
 
+# Create a new S3 bucket with the specified name 'hadia-doing-boto3demo'
 response = s3.create_bucket(
-
     Bucket='hadia-doing-boto3demo'
 )
+
+# Print the creation date of the bucket
 print(response['ResponseMetadata']['HTTPHeaders']['date'])
